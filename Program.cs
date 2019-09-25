2d2f0e2b55ce6471ed00e9f3e0f9a84f571fb64e
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-09-25 18:25:58 -0400

    print env vars to help troubleshooting

--- Program.cs
+++ Program.cs
@@ -1,17 +1,49 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using StackExchange.Redis;
 
 namespace dd_trace_csharp_linux_example
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("Environment variables:");
+
+            foreach (var (key, value) in GetEnvironmentVariables())
+            {
+                Console.WriteLine($" {key}={value}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Connecting to redis...");
+
             using (var redis = ConnectionMultiplexer.Connect("redis"))
             {
+                Console.WriteLine(@"Setting value.");
                 redis.GetDatabase().StringSet("x", "y");
-                Console.WriteLine(redis.GetDatabase().StringGet("x"));
+
+                Console.WriteLine(@"Getting value.");
+                redis.GetDatabase().StringGet("x");
             }
+
+            Console.WriteLine("Done.");
+            SpinWait.SpinUntil(() => false);
+        }
+
+        private static IEnumerable<(string key, string value)> GetEnvironmentVariables()
+        {
+            var prefixes = new[] {"COR_", "CORECLR_", "DD_", "DATADOG_"};
+
+            return from envVar in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>()
+                   from prefix in prefixes
+                   let key = (envVar.Key as string)?.ToUpperInvariant()
+                   where key.StartsWith(prefix)
+                   orderby key
+                   select (key, envVar.Value as string);
         }
     }
 }
